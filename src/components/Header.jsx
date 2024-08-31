3f1e4eb29dbbc1a1b38fb30725eaff87958ffc02
Jayesh445 <Jayesh040405@gmail.com>
2024-08-31 23:14:48 +0530

    UI design

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <select name='language'>
+            <option value=""></option>
+        </select>
+    </div>
+  )
+}
+
+export default Header
